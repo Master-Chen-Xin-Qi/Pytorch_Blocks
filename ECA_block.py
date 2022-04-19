0b3697067458c23fc6b42f5146c489c46f36941e
XinqiChen <chenxq66@mail2.sysu.edu.cn>
2022-04-19 16:18:39 +0800

    ECA similar to se block

---
+++ ECA_block.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File         : ECA_block.py
+@Date         : 2022/04/19 16:17:22
+@Author       : Xinqi Chen 
+@Software     : VScode
+@Description  : ECA block(similar to squeeze and excitation block)
+'''
+
+import math
+import torch
+import torch.nn as nn
+
+
+class ECABlock(nn.Module):
+    def __init__(self, channels, gamma=2, b=1):
+        super(ECABlock, self).__init__()
+        kernel_size = int(abs((math.log(channels, 2) + b) / gamma))
+        kernel_size = kernel_size if kernel_size % 2 else kernel_size + 1
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        v = self.avg_pool(x)
+        v = self.conv(v.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        v = self.sigmoid(v)
+        return x * v
+
+
+if __name__ == "__main__":
+    features_maps = torch.randn((8, 54, 32, 32))
+    model = ECABlock(54, gamma=2, b=1)
+    print(model(features_maps).shape)
